7a0a0e8df003203535d83eb844f085b24e217d8e
Ashwani Gautam <lwmarsh06@gmail.com>
2017-01-08 12:08:15 +0530

    Backtraking problem

---
+++ N-Queen.py
@@ -0,0 +1,78 @@
+import sys
+
+
+def column_check(col):
+    for i in xrange(N):
+        if chessboard[i][col]:
+            return False
+    return True
+
+
+def row_check(row):
+    for i in xrange(N):
+        if chessboard[row][i]:
+            return False
+    return True
+
+
+def diagonal_check(row, col):
+
+    i, j, k, l = row, col, row, col
+
+# Checking left upper diagonal
+    while i >= 0 and j >= 0:
+        if chessboard[i][j]:
+            return False
+        i -= 1
+        j -= 1
+# Checking left lower diagonal        
+    while k < N and l >= 0:
+        if chessboard[k][l]:
+            return False
+        k += 1
+        l -= 1
+        
+    return True
+
+
+def check(row, col):
+
+    if row_check(row) and column_check(col) and diagonal_check(row, col):
+        return True
+    return False
+
+
+def place_queen(row, col):
+    chessboard[row][col] = 1
+
+
+def remove_queen(row, col):
+    chessboard[row][col] = 0
+
+
+def solve(col):
+    global total
+    if col >= N:
+        total += 1
+        return 1
+
+    for i in xrange(N):
+        if check(i, col):
+            place_queen(i, col)
+            solve(col+1)
+            remove_queen(i, col)
+
+    return 0
+
+
+for arg in sys.argv:
+    N = int(sys.argv[1])
+
+chessboard = []
+for ite in xrange(N):
+    chessboard.append([0] * N)
+
+
+total = 0
+solve(0)
+print total
